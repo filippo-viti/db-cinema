d6e2f47b83efc304e2175ef872a06568f0f00e60
filippo-viti <filippo.viti2002@gmail.com>
2020-05-20 09:23:44 +0200

    Replaced table array with getTables()

--- populate.php
+++ populate.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/data.php';
 
 $cinemaData = new CinemaData();
 echo "Emptying tables...\n";
-$tables = ["Attori", "Colonne_Sonore", "Film", "Genere", "Ha_Vinto", "Musicisti", "Premi", "Recita_In"];
+$tables = $cinemaData->getTables();
 foreach($tables as $table) {
     echo ">>$table\n";
     $cinemaData->emptyTable($table);
