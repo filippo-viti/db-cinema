bf4e597d0d02033b1e77f1f361b8c17e33f96642
filippo-viti <filippo.viti2002@gmail.com>
2020-05-21 10:22:24 +0200

    Added list download

--- populate.php
+++ populate.php
@@ -1,6 +1,9 @@
 <?php
 require_once __DIR__ . '/data.php';
+require_once __DIR__ . '/get_list.php';
 
+echo "Downloading valid ID list...\n";
+$movieList = new MovieList();
 $cinemaData = new CinemaData();
 echo "Emptying tables...\n";
 $tables = $cinemaData->getTables();
