0eed76907df43c1fe62386ef2ed8b87e69d15aed
filippo-viti <filippo.viti2002@gmail.com>
2020-05-23 11:05:00 +0200

    Updated population script

--- populate.php
+++ populate.php
@@ -3,7 +3,6 @@ require_once __DIR__ . '/data.php';
 require_once __DIR__ . '/get_list.php';
 
 echo "Downloading valid ID list...\n";
-$movieList = new MovieList();
 $cinemaData = new CinemaData();
 echo "Emptying tables...\n";
 $tables = $cinemaData->getTables();
@@ -11,3 +10,5 @@ foreach ($tables as $table) {
     echo ">>$table\n";
     $cinemaData->emptyTable($table);
 }
+echo "Inserting into table Film...\n";
+$cinemaData->insertDataFilm();
