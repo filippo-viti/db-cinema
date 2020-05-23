852b7bc0cc479c218fe336bce5a0f49ac2f37210
filippo-viti <filippo.viti2002@gmail.com>
2020-05-23 16:40:17 +0200

    Renamed to MovieList

---
+++ MovieList.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__ . '/ExportHandler.php';
+require_once __DIR__ . '/ExportList.php';
+
+class MovieList extends ExportList
+{
+    // set to negative if you want to get all movies
+    private $MAX_MOVIES = 500;
+
+    public function __construct()
+    {
+        parent::__construct();
+        ExportHandler::getExport($this->timestamp, "movie");
+        // list is an array of json objects
+        $file = file("movie_data/movie_id_list.json");
+        $this->list = ExportHandler::parseJSON($file, $this->MAX_MOVIES);
+    }
+}
